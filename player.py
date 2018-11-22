e79828ec298ea62c892dd309d1dd1230c7b27797
Tyler Oar <tyleroar@gmail.com>
2018-11-22 15:54:20 -0500

    player object changes

--- player.py
+++ player.py
@@ -1,5 +1,4 @@
 class Player:
-<<<<<<< HEAD
     def __init__(self,row):
         #name,NBAplayerID,position,avgPoints,salary,team,nextOpponent,nextGameLocation
         #row[2],row[3],row[4],row[8],row[5],row[7],opponent,location)
@@ -10,21 +9,10 @@ class Player:
         self.position = row[4]
         self.avgPoints = row[8]
         self.salary = row[5]
-        self.team = row[7]
-        self.nextOpponent = "nextOpponent"
-        self.nextGameLocation = "nextGameLocation" #should be home or away
-=======
-    ##TODO, should just pass in game info and team abbrev and prase out next opponent and nextgamelocation here instead of callee
-    def __init__(self,name,NBAplayerID,position,avgPoints,salary,team,nextOpponent,nextGameLocation):
-        self.name = name
-        self.NBAplayerID = NBAplayerID
-        self.position = position
-        self.avgPoints = avgPoints
-        self.salary = salary
-        self.team = team
-        self.nextOpponent = nextOpponent
-        self.nextGameLocation = nextGameLocation #should be home or away
->>>>>>> e53f877d0b157d5df46c4f392db3f6b4f1cdf6f9
+        self.team = row[7] #this is the team that the player is on
+        #row[6] = gameInfo
+        self.nextOpponent = getOpponent(row[6],self.team)
+        self.nextGameLocation = getLocation(row[6],self.team)#should be home or away
         self.projection = 0
     def __repr__(self):
         return 'Player({},{},{},{},{},{},{},{})'.format(self.name,self.NBAplayerID,self.position,self.avgPoints,self.salary,self.team,self.nextOpponent,self.nextGameLocation,self.projection)
@@ -36,16 +24,17 @@ class Player:
     ###TODO fix these, this is assuming all team abbreviations are 3 letters long, doesn't hold for GS
 
 def getLocation(gameInfo,teamAbbrev):
-    team1=gameInfo[0:3]#awayteam
-    team2=gameInfo[4:7]#hometeam
+    team1=gameInfo[0:gameInfo.find("@")]#awayteam
+    team2=gameInfogameInfo.find("@"):]#hometeam
     if teamAbbrev==team1:
         return "Away"
     else:
         return "Home"
 
 def getOpponent(gameInfo,TeamAbbrev):
-    team1=gameInfo[0:3]#awayteam
-    team2=gameInfo[4:7]#hometeam
+    #gameinfo example = GS@POR
+    team1=gameInfo[0:gameInfo.find("@")]#awayteam
+    team2=gameInfo[gameInfo.find("@")+1:]#hometeam
     if TeamAbbrev==team1:
         return team2
     else:
