d939a0b7365d68b4f4a91b39614f66461a8742a7
Tyler Oar <tyleroar@gmail.com>
2018-11-25 18:11:02 -0500

    using local nba_api

--- nba_api/stats/library/http.py
+++ nba_api/stats/library/http.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     STATS_HEADERS = {
         'Host': 'stats.nba.com',
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:61.0) Gecko/20100101 Firefox/61.0',
         'Accept': 'application/json, text/plain, */*',
         'Accept-Language': 'en-US,en;q=0.5',
         'Accept-Encoding': 'gzip, deflate, br',
